dfa499f0dd5921210b52673f5653699df31ab765
Arewa100 <olasorexway@gmail.com>
2024-07-16 06:28:40 +0100

    number pattern

---
+++ firstcode/firstassignemt/myjavaassignment/NumberPattern.java
@@ -0,0 +1,13 @@
+public class NumberPattern {
+	public static void main(String[] args) {
+		System.out.println("welcome friend: today is a good day, different pattern....hahaha\n");
+		
+	
+	for(int vertical = 5; vertical >= 1; vertical--) {
+			for(int horizontal = vertical; horizontal >= 1; horizontal--) {
+				System.out.print(horizontal);
+			}
+		System.out.println(" ");
+	}
+}
+}
