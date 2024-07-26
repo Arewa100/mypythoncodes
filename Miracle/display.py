fe629a88a086422530a87dd8edb877d400841d70
Arewa100 <olasorexway@gmail.com>
2024-07-26 12:58:15 +0100

    phase gate one

--- Miracle/display.py
+++ Miracle/display.py
@@ -1,28 +1,23 @@
 #peudo code
 # 1: create a function called display 
 # 2: let the function take in two parameter called a and b
-# 3: inside the function, create a variable called pound_in_kilogram
-# 4: create a constant variable called ONE_POUND_IN_KILOGRAM and assign its value as 0.454
-# 5: multply ONE_POUND_IN_KILOGRAM by the 'pound' and assign its result to the 'pound_in_kilogram'
-# 6: return the pound_in_kilogram
-# 7: prompt user to enter pound to be converted
-# 8: call on function pound_to_kilogram and pass the user input as an argument to it
-# 9: assign it result to a declared variable called 'pound_in_kilogram;
-# 10: print the result of the converted pound
+# 3: inside the function, create a variable called product
+# 4: write a for loop to calculate the product according to the value of 'a'
+# 5: return the result of each iteration
 
 
 
+def display(a, b):	
+	product = (a, b)
+	return product
 
 
-def pound_to_kilogram(pound):
-	ONE_POUND_IN_KILOGRAM = 0.454
-	pound_in_kilogram = (pound * ONE_POUND_IN_KILOGRAM)
+
+a = int(input("Enter first value "))
+b = int(input("Enter second value "))
 	
-	return pound_in_kilogram
-
-
-pound = int(input("Enter the pound to be converted:  "))
+for counter in range(a + 1):
+	result = product(a, b)
 	
-pound_in_kilogram =  pound_to_kilogram(pound)
-print(f"the result of {pound} pound in kilogram is {pound_in_kilogram} kilograms")
+
 
