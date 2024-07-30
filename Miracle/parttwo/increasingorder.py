2fe6dffc097a57ab64b810720a52bd5fc081aaac
Arewa100 <olasorexway@gmail.com>
2024-07-30 11:57:14 +0100

    working on it

---
+++ Miracle/parttwo/increasingorder.py
@@ -0,0 +1,10 @@
+# 1: write a function called get_sortedNumbers
+# 2: pass in three parameter called num_one, num_two, num_three
+# 3: inside the function, create a variable called largest, small and smallest 
+# 4: compare the num_one with num_two, if num_one, compare anyone that is greater between them with num_ three
+# 5: if result from step 3 equal to not zero, return "it is an odd integer"
+# 6: test the function by prompting user for input
+
+
+def get_sortednumbers(num_one, num_two, num_three):
+	
