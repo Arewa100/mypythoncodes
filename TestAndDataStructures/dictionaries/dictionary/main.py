489edc2b1b6ef04efb6b2e7fdc65559f1258a031
Arewa100 <olasorexway@gmail.com>
2024-09-01 19:16:29 +0100

    data structures

--- TestAndDataStructures/dictionaries/dictionary/main.py
+++ TestAndDataStructures/dictionaries/dictionary/main.py
@@ -35,13 +35,13 @@
 
 
 
-# family = {
-#     "father": "Mr Olu",
-#     "mother": "Mrs Olu",
-#     "Children": 4,
-#     "language": "yoruba"
-# }
-
+family = {
+    "father": "Mr Olu",
+    "mother": "Mrs Olu",
+    "Children": 4,
+    "language": "yoruba"
+}
+print(family.items())
 # for key, value in family.items():
 #     #"""this is simply looping through the dictionary"""  this is unpacking
 #     print(key, value, end= ' ')
@@ -55,7 +55,7 @@
 # print(f"this is the new family copied from 'family'\n{new_family}")
 # new_family.clear()
 # print(f"this is the new family copied from 'family' after it is  cleared\n{new_family}")
-# print(family.get("father", "the key does exists"))
+# print(family.get("father", "the key does mot exists"))
 # print(family["father"])
 
 
@@ -69,6 +69,23 @@
 
 # given_value = [1, 2, 3, 4, 5]
 # output = {1: 1, 2: 4, 3: 9, 4: 16, 5: 25}
-from key_multiplier import key_multiplier
-given_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-print(key_multiplier(given_list))
+# from key_multiplier import key_multiplier as km
+# given_list = [1, 2, 3, 4, 5, 6, 7, 10, 9, 10]
+# print(km(given_list))
+#
+#
+#
+# #dictionary comprehension
+#
+#
+# result = {number: number ** 3 for number in range(1, 11)}  #this is called dictionary comprehension
+# print(result)
+#
+# # writing a set
+# my_set = {1, 2, 3, 1, "one", "one", "One"}
+# #"""set function is used to create empty set
+# print(my_set)
+# empty_set = {5, 7, 8}
+# print(my_set | empty_set)
+# my_set.clear()
+# print(my_set)
