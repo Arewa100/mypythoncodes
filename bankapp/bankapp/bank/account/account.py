7c3c3e93b234a81c65ee4409b739e42f87e08152
Arewa100 <olasorexway@gmail.com>
2024-09-01 22:11:28 +0100

    account app

--- bankapp/bankapp/bank/account/account.py
+++ bankapp/bankapp/bank/account/account.py
@@ -1,23 +1,33 @@
+from decimal import Decimal
 class Account:
     def __init__(self, account_number, firstname, lastname):
+        """initialize account object"""
         self.account_number = account_number
         self.firstname = firstname
         self.lastname = lastname
-        self.balance = 0
+        self.balance = Decimal("0")
         self.pin = "0000"
 
     def check_balance(self, account_number, pin):
-        if account_number == self.account_number and pin == self.pin:
+        """method to check the balance of the account"""
+        if account_number == self.account_number and pin == self.pin and self.pin != "0000":
             return self.balance
         else:
             raise ValueError("incorrect pin or account_number...")
 
     def deposit(self, account_number, amount):
-        if amount > 0:
-            self.balance = self.balance + amount
+        """method to deposit money into the account"""
+        if amount > 0 and self.account_number == account_number:
+            self.balance = self.balance + Decimal(amount)
+        elif not self.account_number == account_number:
+            raise ValueError("incorrect account number...")
+        elif amount < 0:
+            raise ValueError("invalid amount...")
 
     def update_pin(self, new_pin):
-        if new_pin == self.pin:
-            raise ValueError("pin is same as old pin, enter a unique pin...")
+        """method to update the pin of the account"""
+        if new_pin == self.pin or len(new_pin) != 4:
+            raise ValueError("ensure pin is 4 digits and not old pin...")
         self.pin = new_pin
+
 
--- bankapp/bankapp/tests/account/account_test.py
+++ bankapp/bankapp/tests/account/account_test.py
@@ -10,33 +10,39 @@ class TestAccount(TestCase):
         self.account.account_number = "5555"
 
     def test_to_check_that_account_balance_is_zero(self):
-        self.assertEqual(self.account.check_balance("5555", "0000"), 0)
+        self.account.update_pin("1222")
+        self.assertEqual(self.account.check_balance("5555", "1222"), 0)
 
     def test_to_deposit_2k_and_check_balance_to_be_2k(self):
         self.account.deposit("5555", 2000)
-        self.assertEqual(self.account.check_balance("5555", "0000"), 2000)
-
-    def test_to_deposit_2k_balance_is_2k_and_deposit_5k_balance_is_still_2k(self):
-        self.account.deposit("5555", 2000)
-        self.assertEqual(self.account.check_balance("5555", "0000"), 2000)
-        self.account.deposit("5555", -5000)
-        self.assertEqual(self.account.check_balance("5555", "0000"), 2000)
-
-    def test_that_password_is_required_to_check_balance(self):
-        self.account.deposit("5555", 2000)
-        self.assertEqual(self.account.check_balance("5555", "0000"), 2000)
-
-    def test_to_change_account_pin(self):
-        self.account.deposit("5555", 2000)
-        self.assertEqual(self.account.check_balance("5555", "0000"), 2000)
         self.account.update_pin("1222")
         self.assertEqual(self.account.check_balance("5555", "1222"), 2000)
 
+    def test_to_deposit_2k_balance_is_2k_and_deposit_5k_balance_is_still_2k(self):
+        self.account.deposit("5555", 2000)
+        self.account.update_pin("1222")
+        self.assertEqual(self.account.check_balance("5555", "1222"), 2000)
+        self.assertRaises(ValueError, self.account.deposit, "5555", -5000)
+        self.assertEqual(self.account.check_balance("5555", "1222"), 2000)
+
+    def test_that_password_is_required_to_check_balance(self):
+        self.account.deposit("5555", 2000)
+        self.account.update_pin("1222")
+        self.assertEqual(self.account.check_balance("5555", "1222"), 2000)
+
+    def test_to_change_account_pin(self):
+        self.account.deposit("5555", 2000)
+        self.account.update_pin("1222")
+        self.assertEqual(self.account.check_balance("5555", "1222"), 2000)
+        self.account.update_pin("1223")
+        self.assertEqual(self.account.check_balance("5555", "1223"), 2000)
+
     def test_to_change_pin_and_check_balance_with_the_old_pin(self):
         self.account.deposit("5555", 2000)
-        self.assertEqual(self.account.check_balance("5555", "0000"), 2000)
         self.account.update_pin("1222")
-        self.assertRaises(ValueError, self.account.check_balance,"5555", "0000")
+        self.assertEqual(self.account.check_balance("5555", "1222"), 2000)
+        self.account.update_pin("1223")
+        self.assertRaises(ValueError, self.account.check_balance,"5555", "1222")
 
     def test_to_raise_error_if_new_pin_is_updated_to_previous_pin(self):
         self.account.update_pin("1222")
@@ -44,9 +50,22 @@ class TestAccount(TestCase):
         self.assertEqual(self.account.check_balance("5555", "1222"), 2000)
         self.assertRaises(ValueError, self.account.update_pin, "1222")
 
-    def test_to_deposit_only_when_account_number_is_valid(self):
+    def test_to_deposit_only_when_account_number_is_not_valid(self):
         self.account.deposit("5555", 2000)
-        self.assertEqual(self.account.check_balance("5555", "0000"), 2000)
-        self.assertRaises(ValueError, self.account.deposit, "5455", 2000)
+        self.account.update_pin("1222")
+        self.assertEqual(self.account.check_balance("5555", "1222"), 2000)
+        self.assertRaises(ValueError, self.account.deposit, "5545", 2000)
+
+    def test_that_length_of_pin_must_be_four(self):
+        self.account.deposit("5555", 2000)
+        self.account.update_pin("1222")
+        self.assertRaises(ValueError,  self.account.update_pin, "1222")
+
+    def test_that_to_check_balance_or_update_pin_default_pin_must_be_changed(self):
+        self.account.deposit("5555", 2000)
+        self.assertRaises(ValueError, self.account.check_balance, "5555", "0000")
+        self.account.update_pin("1222")
+        self.assertEqual(self.account.check_balance("5555", "1222"), 2000)
+   
 
 
