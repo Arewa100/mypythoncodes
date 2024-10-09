133a0453bc72c5f5b26f6ce84f49b748b60f7ab0
Arewa100 <olasorexway@gmail.com>
2024-10-09 12:11:52 +0100

    bike project

---
+++ firstcode/loan/loan/src/loan/loan.py
@@ -0,0 +1,49 @@
+class Loan:
+    def __init__(self, firstname, lastname, annual_interest_rate, loan_amount, duration_of_loan):
+        self.__name = f"{firstname} {lastname}"
+        self.annual_rate = annual_interest_rate
+        self.loan_amount = loan_amount
+        self.loan_duration = duration_of_loan
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def annual_rate(self):
+        return self.__annual_rate
+
+    @annual_rate.setter
+    def annual_rate(self, rate):
+        if self.__is_above_zero(rate):
+            self.__annual_rate = rate
+        else:
+            raise ValueError("rate cannot be negative")
+
+
+    @property
+    def loan_amount(self):
+        return self.__loan_amount
+
+    @loan_amount.setter
+    def loan_amount(self, amount):
+        if self.__is_above_zero(amount):
+            self.__loan_amount = amount
+        else:
+            raise ValueError("Loan amount cannot be negative")
+
+    def __is_above_zero(self, value):
+        return True if value > 0 else False
+
+    @property
+    def loan_duration(self):
+        return self.__loan_duration
+
+    @loan_duration.setter
+    def loan_duration(self, value):
+        if self.__is_above_zero(value):
+            self.__loan_duration = value
+        else: raise ValueError("Loan duration cannot be negative")
+
+    def compute_monthly_payment(self):
+        pass
---
+++ firstcode/loan/loan/tests/loan/testforloan.py
@@ -0,0 +1,32 @@
+from unittest import TestCase
+from src.loan.loan import Loan
+class TestLoan(TestCase):
+    def setUp(self):
+        self.loan: Loan = Loan("Grady", "Boosh", 12, 2000, 1)
+
+    def test_that_loan_exists(self):
+        self.assertEqual(isinstance(self.loan, Loan), True)
+
+    def test_that_borrower_name_has_been_initialised(self):
+        self.assertEqual(self.loan.name, "Grady Boosh")
+
+    def test_that_annual_interest_rate_has_been_initialised(self):
+        self.assertEqual(self.loan.annual_rate, 12)
+
+    def test_that_loan_amount_has_been_set(self):
+        self.assertEqual(self.loan.loan_amount, 2000)
+
+    def test_that_duration_of_loan_has_been_set(self):
+        self.assertEqual(self.loan.loan_duration, 1)
+
+    def test_that_loan_amount_can_not_be_set_to_negative(self):
+        self.assertRaises(ValueError, Loan, "Grady", "Boosh", 12, -2000, 1)
+
+    def test_that_interest_rate_cannot_be_set_to_negative(self):
+        self.assertRaises(ValueError, Loan, "Grady", "Boosh", -12, 2000, 1)
+
+    def test_that_year_can_not_be_set_to_zero(self):
+        self.assertRaises(ValueError, Loan, "Grady", "Boosh", 12, 2000, 0)
+
+    def test_that_monthly_payment_on_loan_can_be_gotten(self):
+        self.assertEqual(self.loan.compute_monthly_payment(), 3000)
