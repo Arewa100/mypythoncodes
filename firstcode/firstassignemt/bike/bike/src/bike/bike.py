390f1c0b927ec8596254009da56b0d98ed2948a9
Arewa100 <olasorexway@gmail.com>
2024-10-10 08:33:28 +0100

    assignment

--- firstcode/firstassignemt/bike/bike/src/bike/bike.py
+++ firstcode/firstassignemt/bike/bike/src/bike/bike.py
@@ -1,12 +1,12 @@
 class Bike:
 
     def __init__(self, gear=1):
-        self.__gear:int = gear
+        self._gear:int = gear
         self.__bike_speed: int = 0
         self.__is_bike_on: bool = False
 
 
-    def is_bike_on(self):
+    def _is_bike_on(self):
         return self.__is_bike_on
 
     def turn_on(self):
@@ -29,3 +29,40 @@ class Bike:
     def _gear(self, gear):
         self.__gear = gear
 
+    def accelerate(self):
+        if self.__is_bike_on:
+            self.accelerate_bike()
+        else:
+            raise ValueError("bike is off")
+
+    def decelerate(self):
+        if self.__is_bike_on:
+           self.decelerate_bike()
+        else:
+            raise ValueError("bike is off")
+
+    def accelerate_bike(self):
+        if self.__bike_speed <= 20:
+            self.__bike_speed += 1
+        elif self.__bike_speed <= 30:
+            self._gear = 2
+            self.__bike_speed += 2
+        elif self.__bike_speed <= 40:
+            self._gear = 3
+            self.__bike_speed += 3
+        elif self.__bike_speed > 41:
+            self._gear = 4
+            self.__bike_speed += 4
+
+    def decelerate_bike(self):
+        if self.__bike_speed <= 20:
+            self.__bike_speed -= 1
+        elif self.__bike_speed <= 30:
+            self._gear = 2
+            self.__bike_speed -= 2
+        elif self.__bike_speed <= 40:
+            self._gear = 3
+            self.__bike_speed -= 3
+        elif self.__bike_speed > 41:
+            self._gear = 4
+            self.__bike_speed -= 4
--- firstcode/firstassignemt/bike/bike/tests/bike/testforbike.py
+++ firstcode/firstassignemt/bike/bike/tests/bike/testforbike.py
@@ -3,34 +3,181 @@ from src.bike.bike import Bike
 
 
 class TestBike(TestCase):
+
+
     def test_that_bike_exists(self):
         bike: Bike = Bike()
         self.assertEqual(isinstance(bike, Bike), True)
 
     def test_that_bike_off_normally(self):
         bike: Bike = Bike()
-        self.assertFalse(bike.is_bike_on())
+        self.assertFalse(bike._is_bike_on())
 
     def test_to_turn_bike_on_when_it_is_off(self):
         bike: Bike = Bike()
         bike.turn_on()
-        self.assertTrue(bike.is_bike_on())
+        self.assertTrue(bike._is_bike_on())
 
     def test_to_turn_bike_off_when_it_is_on(self):
         bike: Bike = Bike()
-        self.assertFalse(bike.is_bike_on())
+        self.assertFalse(bike._is_bike_on())
         bike.turn_on()
-        self.assertTrue(bike.is_bike_on())
+        self.assertTrue(bike._is_bike_on())
         bike.turn_off()
-        self.assertFalse(bike.is_bike_on())
+        self.assertFalse(bike._is_bike_on())
 
     def test_that_current_speed_off_bike_is_zero_when_it_is_off(self):
-        bike: Bike = Bike()
-        self.assertFalse(bike.is_bike_on())
-        self.assertEqual(bike._bike_speed, 0)
+        bike: Bike = Bike(1)
+        self.assertFalse(bike._is_bike_on())
+        self.assertEqual(bike._bike_speed(), 0)
 
-    # def test_to_check_bike_gear_when_it_is_off(self):
-    #     bike: Bike = Bike(1)
-    #     self.assertEqual(bike._gear, "bike is off")
+    def test_to_check_bike_gear_when_it_is_off(self):
+        bike: Bike = Bike(1)
+        self.assertEqual(bike._gear, "bike is off")
+
+    def test_to_check_bike_gear_when_it_is_on(self):
+        bike: Bike = Bike(1)
+        self.assertFalse(bike._is_bike_on())
+        self.assertEqual(bike._gear, "bike is off")
+        bike.turn_on()
+        self.assertTrue(bike._is_bike_on())
+        self.assertEqual(bike._gear, 1)
+
+    def test_to_accelerate_bike_when_it_is_off(self):
+        bike: Bike = Bike(1)
+        self.assertFalse(bike._is_bike_on())
+        self.assertEqual(bike._gear, "bike is off")
+        self.assertRaises(ValueError, bike.accelerate)
+
+    def test_to_accelerate_when_bike_is_on(self):
+        bike: Bike = Bike(1)
+        self.assertFalse(bike._is_bike_on())
+        self.assertEqual(bike._gear, "bike is off")
+        bike.turn_on()
+        self.assertTrue(bike._is_bike_on())
+        bike.accelerate()
+        self.assertEqual(bike._bike_speed(), 1)
+
+    def test_to_decelerate_bike_when_it_is_off(self):
+        bike: Bike = Bike(1)
+        self.assertFalse(bike._is_bike_on())
+        self.assertEqual(bike._gear, "bike is off")
+        self.assertRaises(ValueError, bike.decelerate)
+
+    def test_to_accelerate_and_decelerate_bike_when_it_is_on(self):
+        bike: Bike = Bike(1)
+        self.assertFalse(bike._is_bike_on())
+        bike.turn_on()
+        self.assertTrue(bike._is_bike_on())
+        self.assertEqual(bike._bike_speed(), 0)
+        bike.accelerate()
+        self.assertEqual(bike._bike_speed(), 1)
+        bike.decelerate()
+        self.assertEqual(bike._bike_speed(), 0)
+
+    def test_to_decelerate_bike_speed_to_minimum_speed_which_is_zero(self):
+        bike: Bike = Bike(1)
+        self.assertFalse(bike._is_bike_on())
+        bike.turn_on()
+        self.assertTrue(bike._is_bike_on())
+        bike.decelerate()
+        self.assertEqual(bike._bike_speed(), 0)
+
+    def test_to_accelerate_bike_at_gear_one_and_speed_should_increase_by_1_steps(self):
+        bike: Bike = Bike(1)
+        self.assertFalse(bike._is_bike_on())
+        bike.turn_on()
+        bike.accelerate()
+        self.assertEqual(bike._bike_speed(), 1)
+        bike.accelerate()
+        self.assertEqual(bike._bike_speed(), 2)
+
+    def test_to_accelerate_bike_until_gear_changes_to_gear_two_when_speed_is_21(self):
+        bike: Bike = Bike()
+        bike.turn_on()
+        for acceleration in range(20):
+            bike.accelerate()
+        self.assertEqual(bike._bike_speed(), 20)
+        self.assertEqual(bike._gear, 1)
+        bike.accelerate()
+        self.assertEqual(bike._bike_speed(), 21)
+        bike.accelerate()
+        self.assertEqual(bike._bike_speed(), 23)
+        self.assertEqual(bike._gear, 2)
+        bike.accelerate()
+        self.assertEqual(bike._bike_speed(), 25)
+        self.assertEqual(bike._gear, 2)
+
+    def test_to_accelerate_bike_until_gear_changes_to_gear_three_when_speed_is_above_31(self):
+        bike: Bike = Bike()
+        bike.turn_on()
+        for acceleration in range(26):
+            bike.accelerate()
+        self.assertEqual(bike._gear, 2)
+        self.assertEqual(bike._bike_speed(), 31)
+        bike.accelerate()
+        self.assertEqual(bike._gear, 3)
+        self.assertEqual(bike._bike_speed(), 34)
+        bike.accelerate()
+        self.assertEqual(bike._bike_speed(), 37)
+        bike.accelerate()
+        self.assertEqual(bike._bike_speed(), 40)
+        self.assertEqual(bike._gear, 3)
+
+    def test_to_accelerate_bike_until_gear_changes_to_gear_four_when_speed_is_above_41(self):
+        bike: Bike = Bike()
+        bike.turn_on()
+        for acceleration in range(26):
+            bike.accelerate()
+        self.assertEqual(bike._gear, 2)
+        self.assertEqual(bike._bike_speed(), 31)
+        bike.accelerate()
+        self.assertEqual(bike._gear, 3)
+        self.assertEqual(bike._bike_speed(), 34)
+        bike.accelerate()
+        self.assertEqual(bike._bike_speed(), 37)
+        bike.accelerate()
+        self.assertEqual(bike._bike_speed(), 40)
+        self.assertEqual(bike._gear, 3)
+        bike.accelerate()
+        self.assertEqual(bike._bike_speed(), 43)
+        self.assertEqual(bike._gear, 3)
+        bike.accelerate()
+        self.assertEqual(bike._bike_speed(), 47)
+        self.assertEqual(bike._gear, 4)
+
+    def test_to_decelerate_bike_at_gear_one_and_speed_should_decrease_by_1_steps(self):
+        bike: Bike = Bike(1)
+        self.assertFalse(bike._is_bike_on())
+        bike.turn_on()
+        bike.accelerate()
+        bike.accelerate()
+        self.assertEqual(bike._bike_speed(), 2)
+        bike.decelerate()
+        self.assertEqual(bike._bike_speed(), 1)
+
+    def test_to_decelerate_bike_at_gear_two(self):
+        bike: Bike = Bike()
+        bike.turn_on()
+        for acceleration in range(20):
+            bike.accelerate()
+        self.assertEqual(bike._bike_speed(), 20)
+        self.assertEqual(bike._gear, 1)
+        bike.accelerate()
+        self.assertEqual(bike._bike_speed(), 21)
+        bike.accelerate()
+        self.assertEqual(bike._bike_speed(), 23)
+        self.assertEqual(bike._gear, 2)
+        bike.decelerate()
+        self.assertEqual(bike._bike_speed(), 21)
+        self.assertEqual(bike._gear, 2)
+
+
+
+
+
+
+
+
 
 
