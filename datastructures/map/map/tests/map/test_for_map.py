56909c6fdaa32cb007b10d3df2d936baa07443ba
Arewa100 <olasorexway@gmail.com>
2024-10-14 12:51:25 +0100

    map

--- datastructures/map/map/tests/map/test_for_map.py
+++ datastructures/map/map/tests/map/test_for_map.py
@@ -4,5 +4,5 @@ from src.map.map import Map
 class TestMap(TestCase):
 
     def test_that_map_has_been_created(self):
-        map: Map = Map()
-        self.assertEqual(isinstance(map, Map), True)
+        my_map: Map = Map()
+        self.assertEqual(isinstance(my_map, Map), True)
